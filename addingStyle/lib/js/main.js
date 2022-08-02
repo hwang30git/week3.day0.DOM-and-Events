827ae574e930f22c05ebee4198e0e398df5e629c
Hui Wang <xeraonomen@gmail.com>
2022-08-01 21:48:26 -0400

    finish project except BONUS'

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,7 @@
+let rightSide = document.getElementsByClassName("right_side");
+rightSide[0].style.backgroundColor = "#d3c26f";
+
+let names = document.getElementsByClassName("name");
+for (let name of names) {
+    name.style.textShadow = "-6px 5px 2px";
+}
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,8 @@
+let image = document.getElementById("rengar");
+
+function onClick() {
+    image.src = "assets/rengar.png";
+}
+document.addEventListener("click", onClick);
+
+alert("You've been deleted!");
